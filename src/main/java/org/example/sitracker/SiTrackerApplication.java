9d37f303bfca7e387097a8a2a98aaa453649f9dd
A200268943 <iryna.khrustalova@t-systems.com>
2025-09-13 14:32:00 +0200

    Created SITracker app

---
+++ src/main/java/org/example/sitracker/SiTrackerApplication.java
@@ -0,0 +1,13 @@
+package org.example.sitracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SiTrackerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SiTrackerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/sitracker/SiTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.sitracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SiTrackerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
